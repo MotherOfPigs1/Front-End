f1e111e8c47936e000742a8ed51b2b2484fd9572
Maria Luísa Papahanasiadis Miguel <malumiguel0695@gmail.com>
2023-05-11 17:02:05 -0300

    adição de exercício do módulo 13

---
+++ meu-html-css/13-javascript-ii/scriptexercicio1.js
@@ -0,0 +1,194 @@
+//Exercício 1:
+//a.
+let a = 10;
+let b = 15;
+                
+let soma = a + b;
+                
+console.log(soma);
+        
+//b.
+a = "10";
+b = 2;
+
+soma = a + b;
+
+console.log(soma);
+
+//c.
+a = "10";
+b = 2;
+
+let multiplicacao = a * b;
+
+console.log(multiplicacao);
+
+//d.
+a = "10";
+b = 3;
+
+let divisao = a / b;
+
+console.log(divisao);
+
+//e.
+a = "10";
+b = 3;
+
+let resto = a % b;
+
+console.log(resto);
+
+//f.
+a = 10;
+b = true;
+
+console.log(soma);
+
+//g.
+a = 10;
+b = "10";
+
+console.log(a == b);
+
+//h.
+a = 10;
+b = "10";
+
+console.log(a === b);
+
+//i.
+a = 10;
+b = 11;
+
+console.log(a < b);
+
+//j.
+a = 10;
+b = 12;
+
+console.log(a > b);
+
+//k.
+a = 10;
+b = 10.1;
+
+console.log(a <= b);
+
+//l.
+a = 10;
+b = 9.99;
+
+console.log(a > b);
+
+//m.
+a = 10;
+b = "dez";
+
+console.log(a != b);
+
+//n.
+a = 10;
+b = true;
+
+console.log(a + b);
+
+//o.
+a = "dez";
+b = true;
+
+console.log(a + b);
+
+//p.
+a = 10;
+b = false;
+
+console.log(a + b);
+
+//q.
+a = 10;
+b = false;
+
+console.log(a * b);
+
+//r.
+a = true;
+b = true;
+
+console.log(a + b);
+
+//s.
+a = 10;
+
+console.log(a++);
+
+//t.
+a = 10;
+
+console.log(a--);
+
+//u.
+a = 1;
+b = 1;
+
+console.log(a & b);
+
+//v.
+a = 1;
+b = 0;
+
+console.log(a & b);
+
+//w.
+a = 0;
+b = 0;
+
+console.log(a & b);
+
+//x.
+a = 1;
+b = 0;
+
+console.log(a & b);
+
+//y.
+a = 0;
+b = 1;
+
+console.log(a / b);
+
+//z.
+a = 5;
+b = 5;
+
+let c = 10;
+
+console.log(a + b == c);
+
+//aa.
+a = "5";
+b = "5";
+c = 10;
+
+console.log(a + b == c);
+
+//ab.
+a = "5";
+b = 2;
+c = 9;
+
+console.log(a * b > c);
+
+//ac.
+a = 10;
+b = 10;
+c = 2;
+
+console.log((a + b) * c);
+
+//ad.
+a = 10;
+b = 10;
+c = 2;
+
+console.log(a + b * c);
